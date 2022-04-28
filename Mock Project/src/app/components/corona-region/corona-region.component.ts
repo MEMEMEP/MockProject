27242b6e48d5931096e455f76d2ca0665c8b66fd
LinhPLNHE150512 <linhplnhe150512@fpt.edu.vn>
2022-04-29 00:55:45 +0700

    Updated corona

--- Mock Project/src/app/components/corona-region/corona-region.component.ts
+++ Mock Project/src/app/components/corona-region/corona-region.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HandleService } from 'src/app/shared/services/handle.service';
-import { Corona } from './../../shared/models/corona.model';
+import { Covid19Service } from 'src/app/shared/services/covid19.service';
 
 @Component({
   selector: 'app-corona-region',
@@ -8,23 +7,62 @@ import { Corona } from './../../shared/models/corona.model';
   styleUrls: ['./corona-region.component.css']
 })
 export class CoronaRegionComponent implements OnInit {
-  public coronaData!: Corona[];
-  selectedOption: string = '';
-  CountryHasSelect!: Corona;
+  countries: any;
+  selectedOption: any;
+  deaths = 0;
+  confirmed = 0;
+  reCover = 0;
+  dataAll: any;
+  fatality = '';
+  code = '';
 
-  constructor(private handleService: HandleService) { }
+  constructor(private httpService: Covid19Service) {}
 
   ngOnInit(): void {
-    this.handleService.ongetData().subscribe((data: any) => {
-      this.coronaData = data;
+    this.httpService.getAll().subscribe((data: any) => {
+      this.dataAll = data;
+
+      data.forEach((element: any) => {
+        if (element.countryregion == 'Afghanistan') {
+          this.deaths += element.deaths;
+          this.confirmed += element.confirmed;
+          this.reCover += element.recovered;
+          this.fatality = ((this.deaths * 100) / this.confirmed).toFixed(4);
+        }
+      });
+      this.countries = [
+        ...new Map(
+          data.map((item: any) => [item['countryregion'], item])
+        ).values(),
+      ];
     });
   }
 
-  public getCountry(DATA: Corona[]) {
-    return [...new Set(this.coronaData.map((item: any) => item.countryregion))];
-  }
-  getCountryByName(name: string) {
-    return this.coronaData.filter(item => item.countryregion == name);
-  }
+  getCountry() {
+    this.deaths = 0;
+    this.confirmed = 0;
+    this.reCover = 0;
 
+
+    this.dataAll.forEach((element: any) => {
+      if (element.countryregion == this.selectedOption) {
+        this.deaths += element.deaths;
+        this.confirmed += element.confirmed;
+        this.reCover += element.recovered;
+        this.fatality = ((this.deaths * 100) / this.confirmed).toFixed(4);
+        this.createMessageC(this.selectedOption)
+        this.createMessage( element.countrycode.iso2)
+      }
+      //  console.log(this.dataAll);
+
+    });
+
+
+  }
+  createMessage(code: string) {
+    this.httpService.changeMessage(code);
+  }
+  createMessageC(mess:string){
+    this.httpService.changeMessageC(mess)
+  }
 }
